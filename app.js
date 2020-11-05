9db0476df89053f2717f2fe94a033ae7ab2d51cc
wardis <wardi2976@live.fr>
2020-11-05 23:39:13 +0000

    render 3d scene

--- app.js
+++ app.js
@@ -18,7 +18,23 @@ function init() {
   let controls = new THREE.OrbitControls(camera, renderer.domElement);
   controls.enableZoom = false;
 
+  let urls = [
+    './images/scene_r.jpg',
+    './images/scene_l.jpg',
+    './images/scene_u.jpg',
+    './images/scene_d.jpg',
+    './images/scene_f.jpg',
+    './images/scene_b.jpg',
+  ];
+  let loader = new THREE.CubeTextureLoader();
+  scene.background = loader.load(urls);
+
+  render();
+}
+
+function render() {
   renderer.render(scene, camera);
+  requestAnimationFrame(render);
 }
 
 init();
